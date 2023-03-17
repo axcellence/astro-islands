e5784388d46611ef655ffbc3407ea273a98caaa9
Axl Crawford <hi@axlcrawford.com>
2023-03-17 10:59:10 +0000

    Update astro.config.mjs, package.json, and tsconfig.json to add support for Preact, Solid.js, Tailwind, and Svelte.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), solidJs(), tailwind(), svelte()]
+});
